55e3091c6c31ff9d1e33042e463106986d70ca8e
hamzamahrous <hamza.mahrous11@gmail.com>
2026-02-14 16:56:32 +0200

    feat(navbar): implement navbar component with responsive design and action buttons

--- src/app/core/components/navbar/navbar-component/navbar.component.ts
+++ src/app/core/components/navbar/navbar-component/navbar.component.ts
@@ -6,8 +6,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'soual-navbar',
   imports: [ButtonModule, DrawerModule, RouterLink],
-  templateUrl: './navbar-component.html',
-  styleUrl: './navbar-component.css',
+  templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'block w-full',
