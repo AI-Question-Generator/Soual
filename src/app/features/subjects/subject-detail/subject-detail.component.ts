bf41b35a9e65a511dfbfdaef7d748c4f22ca58a8
hamzamahrous <hamza.mahrous11@gmail.com>
2026-04-10 23:14:53 +0200

    feat(subjects): add initial components for the shell, subject details and initial skeleton for the subjects service

---
+++ src/app/features/subjects/subject-detail/subject-detail.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'soual-subject-detail',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'block' },
+  imports: [],
+  template: ``,
+})
+export class SubjectDetailComponent {}
