a7e5c059950a7f81df5d8eac281cbd79770bdd59
Hamza Mahrous <hamza.mahrous11@gmail.com>
2026-06-30 02:52:55 +0300

    feat(environment): update apiBaseUrl to production backend

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://vcvst8qj-8000.uks1.devtunnels.ms',
+  apiBaseUrl: 'https://grad-project-backend-production.up.railway.app',
 };
