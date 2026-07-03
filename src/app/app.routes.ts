550c24fc924a18b74dcc4863126c2e15034317fa
Hamza Mahrous <hamza.mahrous11@gmail.com>
2026-07-03 15:22:25 +0300

    feat(subjects): integrate subject routes into main application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '@core/guards';
+import { subjectRoutes } from '@feature/subjects/subjects.routes';
 
 export const routes: Routes = [
   {
@@ -17,6 +18,7 @@ export const routes: Routes = [
         loadComponent: () =>
           import('@feature/profile/profile.component').then((m) => m.ProfileComponent),
       },
+      ...subjectRoutes,
     ],
   },
   {
