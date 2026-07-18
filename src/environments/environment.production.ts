fc9a510537daa368f6fc5a51e09b5438b2d12a77
Hamza Mahrous <hamza.mahrous11@gmail.com>
2026-07-18 14:39:16 +0300

    fix(environment): update apiBaseUrl for production environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://grad-project-backend-production.up.railway.app',
+  apiBaseUrl: 'https://grad-project-backend-production-1cc2.up.railway.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://grad-project-backend-production.up.railway.app',
+  apiBaseUrl: 'https://grad-project-backend-production-1cc2.up.railway.app',
 };
